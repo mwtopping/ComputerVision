5ee8719dd5ac46dd08987755366a31f6babf750a
Michael Topping <miketopping1@gmail.com>
2024-08-14 07:51:58 -0700

    "Finished" Ch3 P4

--- Chapter3/p4-Blue_screen/main.go
+++ Chapter3/p4-Blue_screen/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	//	"image/color"
+	"image/color"
 	_ "image/jpeg"
 	"log"
-	//	"math"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -14,6 +14,7 @@ import (
 var img *ebiten.Image
 var img2 *ebiten.Image
 var img3 *ebiten.Image
+var img4 *ebiten.Image
 
 func init() {
 	var err error
@@ -26,30 +27,68 @@ func init() {
 		log.Fatal(err)
 	}
 
-	img3, _, err = ebitenutil.NewImageFromFile("FG.jpg")
+	img3 = ebiten.NewImage(910, 732)
+
+	img4, _, err = ebitenutil.NewImageFromFile("FG.jpg")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 }
 
+func clamp(x, vmin, vmax uint32) (y uint32) {
+	var clamped uint32
+	if x < vmin {
+		clamped = vmin
+	} else if x > vmax {
+		clamped = vmax
+	} else {
+		clamped = x
+	}
+
+	return clamped
+}
+
+func balancecolor(r, g, b uint32, scale, power float64) (nr, ng, nb uint8) {
+
+	// assumes the input jpg is input as with uint32 values for RGB
+	_r := math.Pow(math.Pow(float64(r/256), power)*scale, 1/power)
+	_g := math.Pow(math.Pow(float64(g/256), power)*scale, 1/power)
+	_b := math.Pow(math.Pow(float64(b/256), power)*scale, 1/power)
+
+	rr := uint8(clamp(uint32(_r), 0, 255))
+	gg := uint8(clamp(uint32(_g), 0, 255))
+	bb := uint8(clamp(uint32(_b), 0, 255))
+
+	return rr, gg, bb
+}
+
 type Game struct {
 }
 
 func (g *Game) Update() error {
-	//	for row := range 910 {
-	//		for col := range 732 {
-	//			c := img.At(row, col)
-	//			r, g, b, _ := c.RGBA()
-	//			//			rr, gg, bb := balancecolor(r, g, b, 1.5, 1.0)
-	//			//			newc := color.Color(color.RGBA{rr, gg, bb, 255})
-	//			//			img2.Set(row, col, newc)
-	//			//			rr, gg, bb = balancecolor(r, g, b, 1.5, 2.2)
-	//			//			newc = color.Color(color.RGBA{rr, gg, bb, 255})
-	//			//			img3.Set(row, col, newc)
-	//
-	//		}
-	//	}
+	for row := range 910 {
+		for col := range 732 {
+			c := img.At(row, col)
+			r, g, b, _ := c.RGBA()
+			rr, gg, bb := balancecolor(r, g, b, 1.0, 1.0)
+			dist := math.Sqrt(math.Pow(float64(rr)-0.0, 2) +
+				math.Pow(float64(gg)-0.0, 2) +
+				math.Pow(float64(bb)-255.0, 2))
+
+			if (dist < 270.0) && (col < 500) {
+				newc := color.Color(color.RGBA{255, 255, 255, 255})
+				img3.Set(row, col, newc)
+				c2 := img2.At(row, col)
+				img4.Set(row, col, c2)
+			}
+
+			//			newc := color.Color(color.RGBA{rr, gg, bb, 255})
+			//			img2.Set(row, col, newc)
+			//			rr, gg, bb = balancecolor(r, g, b, 1.5, 2.2)
+
+		}
+	}
 	fmt.Println("Updated!")
 	return nil
 }
@@ -62,18 +101,22 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(img2, op)
 
 	op2 := &ebiten.DrawImageOptions{}
-	op2.GeoM.Translate(910*2, 0)
+	op2.GeoM.Translate(0, 732)
 	screen.DrawImage(img3, op2)
+
+	op3 := &ebiten.DrawImageOptions{}
+	op3.GeoM.Translate(910, 732)
+	screen.DrawImage(img4, op3)
 
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 910 * 3, 732
+	return 910 * 2, 732 * 2
 }
 
 func main() {
 	fmt.Println("It's starting!")
-	ebiten.SetWindowSize(910*3/2, 732/2)
+	ebiten.SetWindowSize(910, 732)
 	ebiten.SetWindowTitle("Problem 1; Color Balance")
 	ebiten.SetTPS(0)
 	err := ebiten.RunGame(&Game{})
