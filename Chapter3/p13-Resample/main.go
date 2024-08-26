2e2756886716ac63e0006507f7f74d2cb6ca1fe8
Michael Topping <miketopping1@gmail.com>
2024-08-26 16:49:10 -0700

    Completed interpolation/decimation (P13)

--- Chapter3/p13-Resample/main.go
+++ Chapter3/p13-Resample/main.go
@@ -43,6 +43,122 @@ func pad(origImg *ebiten.Image, pad int) (paddedImage *ebiten.Image) {
 	return newimg
 }
 
+func spline(x float64) (y float64) {
+	a := -0.5
+	if x < 1 {
+		return 1 - (a+3)*math.Pow(x, 2) + (a+2)*math.Pow(x, 3)
+	} else if x < 2 {
+		return a * (x - 1) * (x - 2) * (x - 2)
+	} else {
+		return 0
+	}
+}
+
+func interp(inpImg *ebiten.Image) (outImg *ebiten.Image) {
+
+	p := inpImg.Bounds().Size()
+	w := p.X
+	h := p.Y
+	r := 2
+
+	highResImg := ebiten.NewImage(r*w, r*h)
+
+	for row := range r * w {
+		for col := range r * h {
+			//			frow := float64(row)
+			//			fcol := float64(col)
+			finalr := float64(0)
+			totkern := float64(0)
+			//			finalb := int32(0)
+			//			finalg := int32(0)
+			//			fmt.Println("For Pixel ", row, col)
+			for ii := -3; ii < 4; ii++ {
+				for jj := -3; jj < 4; jj++ {
+					c := inpImg.At(((row/r+ii)+w)%w, ((col/r+jj)+h)%h)
+					red, _, _, _ := c.RGBA()
+					dx := float64(ii) / float64(r)
+					dy := float64(jj) / float64(r)
+					dist := math.Sqrt(math.Pow(dx, 2) + math.Pow(dy, 2))
+					//					fmt.Println(dx, dy, dist, spline(dist))
+					sval := spline(dist)
+					finalr += sval * float64(red)
+					totkern += sval
+
+					//					finalg += kernel[ii][jj] * int32(g/256)
+					//					finalb += kernel[ii][jj] * int32(b/256)
+				}
+
+			}
+			ifinalr := int32(finalr / totkern / 256)
+			//			finalg /= int32(256)
+			//			finalb /= int32(256)
+			//
+			newc := color.Color(color.RGBA{uint8(clamp(ifinalr, 0, 255)),
+				uint8(clamp(ifinalr, 0, 255)),
+				uint8(clamp(ifinalr, 0, 255)),
+				255})
+			highResImg.Set(row, col, newc)
+		}
+	}
+
+	return highResImg
+}
+
+func decimate(inpImg *ebiten.Image) (outImg *ebiten.Image) {
+
+	p := inpImg.Bounds().Size()
+	w := p.X
+	h := p.Y
+	r := 10
+
+	kernel := [5][5]int32{
+		{1, 4, 6, 4, 1},
+		{4, 16, 24, 16, 4},
+		{6, 24, 36, 24, 6},
+		{4, 16, 24, 16, 4},
+		{1, 4, 6, 4, 1},
+	}
+
+	lowResImg := ebiten.NewImage(w/r, h/r)
+
+	for row := range w / r {
+		for col := range h / r {
+			//			frow := float64(row)
+			//			fcol := float64(col)
+			finalr := float64(0)
+			totkern := float64(0)
+			//			finalb := int32(0)
+			//			finalg := int32(0)
+			//			fmt.Println("For Pixel ", row, col)
+			for ii := -2; ii < 3; ii++ {
+				for jj := -2; jj < 3; jj++ {
+					c := inpImg.At(((r*row+ii)+w)%w, ((r*col+jj)+h)%h)
+					red, _, _, _ := c.RGBA()
+					//					fmt.Println(dx, dy, dist, spline(dist))
+					sval := kernel[ii+2][jj+2]
+					finalr += float64(sval) * float64(red)
+					totkern += float64(sval)
+
+					//					finalg += kernel[ii][jj] * int32(g/256)
+					//					finalb += kernel[ii][jj] * int32(b/256)
+				}
+
+			}
+			ifinalr := int32(finalr / totkern / 256)
+			//			finalg /= int32(256)
+			//			finalb /= int32(256)
+			//
+			newc := color.Color(color.RGBA{uint8(clamp(ifinalr, 0, 255)),
+				uint8(clamp(ifinalr, 0, 255)),
+				uint8(clamp(ifinalr, 0, 255)),
+				255})
+			lowResImg.Set(row, col, newc)
+		}
+	}
+
+	return lowResImg
+}
+
 func clamp(x, vmin, vmax int32) (y int32) {
 	var clamped int32
 	if x < vmin {
@@ -56,156 +172,14 @@ func clamp(x, vmin, vmax int32) (y int32) {
 	return clamped
 }
 
-//
-//func balancecolor(r, g, b uint32, scale, power float64) (nr, ng, nb uint8) {
-//
-//	// assumes the input jpg is input as with uint32 values for RGB
-//	_r := math.Pow(math.Pow(float64(r/256), power)*scale, 1/power)
-//	_g := math.Pow(math.Pow(float64(g/256), power)*scale, 1/power)
-//	_b := math.Pow(math.Pow(float64(b/256), power)*scale, 1/power)
-//
-//	rr := uint8(clamp(uint32(_r), 0, 255))
-//	gg := uint8(clamp(uint32(_g), 0, 255))
-//	bb := uint8(clamp(uint32(_b), 0, 255))
-//
-//	return rr, gg, bb
-//}
-
 type Game struct {
-	finalImg *ebiten.Image
-	gaussImg *ebiten.Image
-}
-
-func gaussianConvolve(inpImg *ebiten.Image, kernelsize int) (outputimg *ebiten.Image) {
-
-	paddedImg := pad(inpImg, (kernelsize-1)/2)
-
-	p := paddedImg.Bounds().Size()
-	w := p.X
-	h := p.Y
-
-	padsize := (kernelsize - 1) / 2
-	himg := ebiten.NewImage(w-2*padsize, h-2*padsize)
-
-	kernel := [5][5]int32{
-		{1, 4, 6, 4, 1},
-		{4, 16, 24, 16, 4},
-		{6, 24, 36, 24, 6},
-		{4, 16, 24, 16, 4},
-		{1, 4, 6, 4, 1},
-	}
-
-	for row := range w {
-		for col := range h {
-
-			finalr := int32(0)
-			finalb := int32(0)
-			finalg := int32(0)
-			//			fmt.Println("For Pixel ", row, col)
-			for ii := range kernelsize {
-				for jj := range kernelsize {
-					c := paddedImg.At(row+padsize+ii-kernelsize/2, col+padsize+jj-kernelsize/2)
-					r, g, b, _ := c.RGBA()
-					finalr += kernel[ii][jj] * int32(r/256)
-					finalg += kernel[ii][jj] * int32(g/256)
-					finalb += kernel[ii][jj] * int32(b/256)
-				}
-
-			}
-
-			finalr /= int32(256)
-			finalg /= int32(256)
-			finalb /= int32(256)
-
-			newc := color.Color(color.RGBA{uint8(clamp(finalr, 0, 255)),
-				uint8(clamp(finalr, 0, 255)),
-				uint8(clamp(finalr, 0, 255)),
-				255})
-			himg.Set(row, col, newc)
-		}
-	}
-
-	return himg
-}
-
-func kernelsum(kernel [5][5]float64, kernelsize int) (sum float64) {
-	total := float64(0)
-	for ii := range kernelsize {
-		for jj := range kernelsize {
-			total += kernel[ii][jj]
-		}
-	}
-	return total
-}
-
-func bilateralFilter(inpImg *ebiten.Image, kernelsize int) (outputimg *ebiten.Image) {
-
-	paddedImg := pad(inpImg, (kernelsize-1)/2)
-
-	p := paddedImg.Bounds().Size()
-	w := p.X
-	h := p.Y
-
-	fmt.Println("Starting Horizontal Convolution")
-	padsize := (kernelsize - 1) / 2
-	himg := ebiten.NewImage(w-2*padsize, h-2*padsize)
-
-	var kernel [5][5]float64
-	sigmad := 2.5
-	sigmar := 40.0
-
-	for row := range w {
-		for col := range h {
-			c := paddedImg.At(row, col)
-			ithisr, _, _, _ := c.RGBA()
-			thisr := float64(ithisr / 256)
-
-			finalr := float64(0)
-			finalb := float64(0)
-			finalg := float64(0)
-			//			fmt.Println("For Pixel ", row, col)
-			for ii := range kernelsize {
-				for jj := range kernelsize {
-					c := paddedImg.At(row+padsize+ii-kernelsize/2, col+padsize+jj-kernelsize/2)
-					ithatr, _, _, _ := c.RGBA()
-					thatr := float64(ithatr / 256)
-
-					domainval := -0.5 * (math.Pow(float64(ii), 2) +
-						math.Pow(float64(jj), 2)) / math.Pow(sigmad, 2)
-					rangeval := -0.5 * (math.Pow(thisr-thatr, 2)) / math.Pow(sigmar, 2)
-
-					exponent := domainval + rangeval
-
-					kernelval := math.Exp(exponent)
-					kernel[ii][jj] = kernelval
-					//					fmt.Println(thisr, thatr, rangeval)
-				}
-			}
-			ksum := kernelsum(kernel, 5)
-
-			for ii := range kernelsize {
-				for jj := range kernelsize {
-					c := paddedImg.At(row+padsize+ii-kernelsize/2, col+padsize+jj-kernelsize/2)
-					r, g, b, _ := c.RGBA()
-					finalr += kernel[ii][jj] * float64(r/256)
-					finalg += kernel[ii][jj] * float64(g/256)
-					finalb += kernel[ii][jj] * float64(b/256)
-				}
-
-			}
-			newc := color.Color(color.RGBA{uint8(clamp(int32(finalr/ksum), 0, 255)),
-				uint8(clamp(int32(finalr/ksum), 0, 255)),
-				uint8(clamp(int32(finalr/ksum), 0, 255)),
-				255})
-			himg.Set(row, col, newc)
-		}
-	}
-	return himg
+	biggerImg  *ebiten.Image
+	smallerImg *ebiten.Image
 }
 
 func (g *Game) Update() error {
-	g.finalImg = bilateralFilter(img, 2*padval+1)
-	g.gaussImg = gaussianConvolve(img, 2*padval+1)
+	g.biggerImg = interp(img)
+	g.smallerImg = decimate(img)
 
 	fmt.Println("Updated!")
 	return nil
@@ -216,12 +190,14 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(img, nil)
 
 	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(0.5, 0.5)
 	op.GeoM.Translate(320, 0)
-	screen.DrawImage(g.gaussImg, op)
+
+	screen.DrawImage(g.biggerImg, op)
 
 	op = &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(640, 0)
-	screen.DrawImage(g.finalImg, op)
+	screen.DrawImage(g.smallerImg, op)
 
 }
 
@@ -232,7 +208,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	fmt.Println("It's starting!")
-	ebiten.SetWindowSize(320*4.5, 320*1.5)
+	ebiten.SetWindowSize(320*6, 320*2)
 	ebiten.SetWindowTitle("Problem 12; Bilateral Filtering")
 	ebiten.SetTPS(0)
 	err := ebiten.RunGame(&Game{})
